4953b80fb6ff313547ed8debb6fa1f537e114ae0
hl <786676631@qq.com>
2019-07-31 17:43:56 +0800

    新建项目

---
+++ diOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  diOSDemo
+//
+//  Created by 雨浓科技 on 2019/7/31.
+//  Copyright © 2019年 hl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ diOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  diOSDemo
+//
+//  Created by 雨浓科技 on 2019/7/31.
+//  Copyright © 2019年 hl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
